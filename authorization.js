85f4c427be56311f155e2d73f233cca1a134467b
Dog Knife <66633587+Duued@users.noreply.github.com>
2022-11-30 19:36:04 -0500

    Update authorization.js

--- authorization.js
+++ authorization.js
@@ -1,8 +1,11 @@
-function checkAuth(){
-  if document.getElementById("pass").value = "cYDDBpf"{
-    document.getElementById("iframe").style.src = "https://teachers.beauty"
-  }
-  else{
-    alert("Incorrect")
-  }
-}
+      function checkAuth(){
+        if (document.getElementById("pass").value == "cyDDBPf"){
+          document.getElementById("iframe").src = "https://teachers.beauty";
+          document.getElementById("destroy").remove();
+        }
+        else{
+          document.getElementById("destroy").remove()
+          document.getElementById("iframe").remove();
+          document.getElementById("hidden").innerText = "Unauthorized."
+        }
+      }
